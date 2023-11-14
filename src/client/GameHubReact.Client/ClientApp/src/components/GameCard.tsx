6d56af6a3fdd648027ef188834e2a49cd571a07a
Gbenro Selere <seleregb@gmail.com>
2023-11-14 17:20:50 -0400

    displayed platform icons

--- src/client/GameHubReact.Client/ClientApp/src/components/GameCard.tsx
+++ src/client/GameHubReact.Client/ClientApp/src/components/GameCard.tsx
@@ -1,5 +1,6 @@
 import {Game} from "../hooks/useGames";
 import {Card, CardBody, Heading, Image} from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList.tsx";
 
 interface Props {
     game: Game;
@@ -11,6 +12,7 @@ const GameCard = ({game}: Props) => {
           <Image src={game.background_image}></Image>
           <CardBody>
               <Heading fontSize={'2xl'}>{game.name}</Heading>
+              <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
           </CardBody>
       </Card>  
     );
