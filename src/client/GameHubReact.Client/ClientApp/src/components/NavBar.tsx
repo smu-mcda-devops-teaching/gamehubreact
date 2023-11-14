20c771b06481da663d3e33ecec5a0eddbc335450
Gbenro Selere <seleregb@gmail.com>
2023-11-14 16:32:36 -0400

    build color mode switch

---
+++ src/client/GameHubReact.Client/ClientApp/src/components/NavBar.tsx
@@ -0,0 +1,14 @@
+import {HStack, Image} from "@chakra-ui/react";
+import logo from '../assets/logo.webp'
+import ColorModeSwitch from "./ColorModeSwitch.tsx";
+
+const NavBar = () => {
+  return (
+      <HStack>
+          <Image src={logo} boxSize={'60px'}></Image>
+          <ColorModeSwitch />
+      </HStack>
+  );
+}
+
+export default NavBar;
