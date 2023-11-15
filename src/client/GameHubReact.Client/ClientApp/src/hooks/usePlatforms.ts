1aeddf58d2fa816fdd8255f86f7265a93eea0e8c
Gbenro Selere <seleregb@gmail.com>
2023-11-15 01:05:05 -0400

    switch to static data

--- src/client/GameHubReact.Client/ClientApp/src/hooks/usePlatforms.ts
+++ src/client/GameHubReact.Client/ClientApp/src/hooks/usePlatforms.ts
@@ -1,11 +1,5 @@
-import useData from "./useData.ts";
+import platforms from '../data/platforms.ts'
 
-interface Platform {
-    id: number;
-    name: string;
-    slug: string;
-}
-
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+const usePlatforms = () => ({ data: platforms,  isLoading: false, error: null});
 
 export default usePlatforms;
