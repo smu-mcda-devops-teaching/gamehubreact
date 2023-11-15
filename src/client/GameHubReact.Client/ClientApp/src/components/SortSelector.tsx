e6939ccfe44f087be50dc2a9217ea4551bdc752a
Gbenro Selere <seleregb@gmail.com>
2023-11-14 23:56:07 -0400

    build sort selector

---
+++ src/client/GameHubReact.Client/ClientApp/src/components/SortSelector.tsx
@@ -0,0 +1,22 @@
+import {Button, Menu, MenuButton, MenuItem, MenuList} from "@chakra-ui/react";
+import {BsChevronDown} from "react-icons/bs";
+
+interface Props {
+    
+}
+
+const SortSelector = () => {
+
+    return (
+        <Menu>
+            <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+               Order by: Relevance
+            </MenuButton>
+            <MenuList>
+                
+            </MenuList>
+        </Menu>
+    )
+}
+
+export default SortSelector;
