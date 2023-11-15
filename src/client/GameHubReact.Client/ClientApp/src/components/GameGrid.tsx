7d3c6af7215bf7304fd1b70a57da2d91225a826e
Gbenro Selere <seleregb@gmail.com>
2023-11-15 01:11:12 -0400

    refactor game grid

--- src/client/GameHubReact.Client/ClientApp/src/components/GameGrid.tsx
+++ src/client/GameHubReact.Client/ClientApp/src/components/GameGrid.tsx
@@ -10,20 +10,19 @@ interface Props {
 
 const GameGrid = ({gameQuery}: Props) => {
     const {data, error, isLoading} = useGames(gameQuery);
-    const skeletons = [1,2,3,4,5,6];
-    
+    const skeletons = [1, 2, 3, 4, 5, 6];
+
+    if (error) return <Text>{error}</Text>
+
     return (
-        <>
-            {error && <Text>{error}</Text>}
-            <SimpleGrid columns={{
-                sm: 1, md: 2, lg: 3, xl: 4
-            }} padding={'10px'} spacing={6}>
-                {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton} />)}
-                {data.map(game => (
-                    <GameCard key={game.id} game={game}></GameCard>
-                ))}
-            </SimpleGrid>
-        </>
+        <SimpleGrid columns={{
+            sm: 1, md: 2, lg: 3, xl: 4
+        }} padding={'10px'} spacing={6}>
+            {isLoading && skeletons.map(skeleton => <GameCardSkeleton key={skeleton}/>)}
+            {data.map(game => (
+                <GameCard key={game.id} game={game}></GameCard>
+            ))}
+        </SimpleGrid>
     )
 }
 
