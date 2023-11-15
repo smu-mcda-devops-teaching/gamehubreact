e8ba9ede57347d85d7a0dd58939182304bc321cf
Gbenro Selere <seleregb@gmail.com>
2023-11-15 00:16:15 -0400

    sort the games

--- src/client/GameHubReact.Client/ClientApp/src/App.tsx
+++ src/client/GameHubReact.Client/ClientApp/src/App.tsx
@@ -11,6 +11,7 @@ import SortSelector from "./components/SortSelector.tsx";
 export interface GameQuery {
     genre: Genre | null;
     platform: Platform | null;
+    sortOrder: string;
 }
 
 function App() {
@@ -36,9 +37,8 @@ function App() {
         <GridItem area='main' bg='dodgerblue'>
             <HStack spacing={5} paddingLeft={2} marginBottom={5}>
                 <PlatformSelector selectedPlatform={gameQuery.platform} onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
-                <SortSelector />
+                <SortSelector selectedSortOrder={gameQuery.sortOrder} onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})} />
             </HStack>
-            
             <GameGrid gameQuery={gameQuery}/>
         </GridItem>
       </Grid>
